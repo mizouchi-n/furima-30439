77f5b4c1349baaf5cc917abbec6af5155341c152
mizouchi-n <mangetsu212@icloud.com>
2020-10-25 19:05:50 +0900

    ユーザー管理機能変更４回目

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
     factory :user do
         nickname {Faker::Name.initials(number: 2)}
         email {Faker::Internet.free_email}
-        password = {"1a2b3c",Internet.password(min_length: 6)}
-        password {password}
-        password_confirmation {password}
+        password {"1a2b3c"}
+        password_confirmation {"1a2b3c"}
         las_name {"田中"}
         fir_name {"太郎"}
         las_name_r {"タナカ"}
